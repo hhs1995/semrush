43b9bb621e24072e897c1e1f4473259b9b164b00
scherbanich <filippf@bk.ru>
2015-05-27 13:57:59 +0300

    Добавил дамп бд и README
    + небольшие правки: убрал лишний пакет из композера и тд

--- core/App.php
+++ core/App.php
@@ -6,13 +6,25 @@ class App
 {
     private static $container;
 
+    /**
+     * Метод установки DI контейнера
+     *
+     * @param \DI\ContainerInterface $id
+     * @return void
+     */
     public static function setDI(\DI\ContainerInterface $di)
     {
-
         self::$container = $di;
     }
 
-    public static function getDI(){
+
+    /**
+     * Метод получения DI контейнера
+     *
+     * @return \DI\ContainerInterface
+     */
+    public static function getDI()
+    {
 
         return self::$container;
     }
